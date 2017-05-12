e51475bad49110fa13cb8c88aa2d3104faff682d
stevenlomas <ldsiphons@hotmail.com>
2017-05-12 16:33:35 +1000

    Added localized definitions for OmniCC strings
    OmniCC: Fixed missing definitions for day, hour, and minute letter abbreviations.

---
+++ !OmniCC/localization.lua
@@ -0,0 +1,15 @@
+--[[ Default / English localization for OmniCC
+
+To create a localization in your language use the following
+format and add it to OmniCC.toc:
+
+if ( GetLocale() == "localeID" ) then
+`DESCRIPTOR` = "text";
+...
+end
+
+]]
+
+DAY_ONELETTER_ABBR = "d";
+HOUR_ONELETTER_ABBR = "h";
+MINUTE_ONELETTER_ABBR = "m";
